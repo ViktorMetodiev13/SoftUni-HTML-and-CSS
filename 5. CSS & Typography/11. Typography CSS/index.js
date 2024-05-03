67a260c793c3a2971189a648b7c12bd199ef6250
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-05-04 02:47:55 +0300

    Changes

--- 5. CSS & Typography/11. Typography CSS/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("clickMeButton").addEventListener("click", onClick);
-let p = document.getElementById("emptyParagraph");
-
-function onClick(e) {
-    e.preventDefault;
-    p.textContent = 'I am clicked';
-}
